abfbb819ab5b0470a62e27cfc72f78474139d81a
Alerydesu <alerydesu@gmail.com>
2025-08-20 17:08:23 +0900

    トップページをエディットしてサービスの一覧表を準備しておきました

---
+++ JS.js
@@ -0,0 +1,24 @@
+//スライダー
+window.addEventListener('DOMContentLoaded', () => {
+  const range = document.getElementById('range');
+  range.addEventListener('input', () => {
+    document.body.style.setProperty('--pos', `${range.value}%`);
+  });
+});
+
+
+//Go to Up Button
+
+const scrollBtn = document.getElementById('scroll_top_btn');
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
+});
